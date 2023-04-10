b627823086a3e9ed41bf41e6a8b05ce7473ae9a7
RameesAP <rameeswork@gmail.com>
2023-04-11 03:45:01 +0530

    Routes created

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
+import React from 'react';
 import './App.css';
+
+import Username from './components/Username';
+import Reset from './components/Reset';
+import Register from './components/Register';
+import Recovery from './components/Recovery';
+import Profile from './components/Profile';
+import Password from './components/Password';
+import PageNotFound from './components/PageNotFound';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+//root routes
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Username />} />
+        <Route path='/reset' element={<Reset />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/recovery' element={<Recovery />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/password' element={<Password />} />
+        <Route path='/pageNotFound' element={<PageNotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
